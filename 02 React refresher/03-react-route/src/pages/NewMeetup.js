6b4580846d948ba64f33abdafe5bdc3e92303932
florian autricque <fautricque@gmail.com>
2023-09-20 16:48:29 -0400

    fetch data and post data

--- 02 React refresher/03-react-route/src/pages/NewMeetup.js
+++ 02 React refresher/03-react-route/src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+// import { useEffect, useState } from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
+import { useHistory } from "react-router-dom";
+
 function NewMeetup() {
+  const history = useHistory();
   function addMeetUpHandler(meetupData) {
     fetch(
       "https://react-starter-3f273-default-rtdb.firebaseio.com/meetups.json",
@@ -11,8 +15,48 @@ function NewMeetup() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
+
+  //using useEffect
+
+  /*
+  const [meetupData, setMeetupData] = useState(null);
+  useEffect(() => {
+    async function addMeetUpHandler(meetupData) {
+      try {
+        const response = await fetch(
+          "https://react-starter-3f273-default-rtdb.firebaseio.com/meetups.json",
+          {
+            method: "POST",
+            body: JSON.stringify(meetupData),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Error adding meetup");
+        }
+
+        history.replace("/");
+      } catch (error) {
+        console.error("Error adding meetup:", error);
+      }
+    }
+
+    if (meetupData) {
+      addMeetUpHandler(meetupData);
+    }
+  }, [meetupData, history]);
+
+  function handledata(meetupData) {
+    console.log(meetupData);
+    setMeetupData(meetupData);
+  } */
 
   return (
     <section>
