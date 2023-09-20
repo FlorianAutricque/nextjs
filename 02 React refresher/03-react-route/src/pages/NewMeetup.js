04aa0bf68cd80781d6c2a4c5b8a31851b5c7b12b
florian autricque <fautricque@gmail.com>
2023-09-20 15:09:04 -0400

    new meetup and backend logic

--- 02 React refresher/03-react-route/src/pages/NewMeetup.js
+++ 02 React refresher/03-react-route/src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetup() {
+  function addMeetUpHandler(meetupData) {
+    fetch(
+      "https://react-starter-3f273-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetUpHandler} />
     </section>
   );
 }
