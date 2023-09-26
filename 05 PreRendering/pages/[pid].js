b9473aed0e5c538bbb0342553b83b6df4fdf7a1a
florian autricque <fautricque@gmail.com>
2023-09-26 13:10:33 -0400

    getstaticpaths

--- 05 PreRendering/pages/[pid].js
+++ 05 PreRendering/pages/[pid].js
@@ -2,13 +2,18 @@ import { Fragment } from "react";
 import path from "path";
 import fs from "fs/promises";
 
-function ProductDetailPage() {
+function ProductDetailPage(props) {
   const { loadedProduct } = props;
+
+  //need this fallback, guardcause, if we set fallback to true
+  // if (!loadedProduct) {
+  //   return <p>Loading...</p>;
+  // }
 
   return (
     <Fragment>
       <h1>{loadedProduct.title} </h1>
-      <p>{loadedProduct.descitption}</p>
+      <p>{loadedProduct.description}</p>
     </Fragment>
   );
 }
@@ -28,6 +33,20 @@ export async function getStaticProps(context) {
     props: {
       loadedProduct: product,
     },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        //because in the data there is 3 p
+        params: { pid: "p1" },
+      },
+    ],
+
+    //fallback is set to true if we want only to preload, pre generate some pages, not all of them. The pages that are the most visited
+    fallback: "blocking",
   };
 }
 
