e3d9b16cb4abd602ee3f4d2a17e076d402cd1b63
florian autricque <fautricque@gmail.com>
2023-09-26 17:14:58 -0400

    opt data fetching

--- 04 Project Pages/project/pages/events/[eventId].js
+++ 04 Project Pages/project/pages/events/[eventId].js
@@ -2,7 +2,7 @@ import EventContent from "@/components/event-detail/event-content";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventSummary from "@/components/event-detail/event-summary";
 import ErrorAlert from "@/components/ui/error-alert";
-import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import { Fragment } from "react";
 
 function EventDetailPage({ selectedEvent }) {
@@ -10,9 +10,9 @@ function EventDetailPage({ selectedEvent }) {
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -41,17 +41,18 @@ export async function getStaticProps(context) {
     props: {
       selectedEvent: event,
     },
+    revalidate: 30,
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
 
   const paths = events.map(event => ({ params: { eventId: event.id } }));
 
   return {
     paths: paths,
-    fallback: false,
+    fallback: "blocking",
   };
 }
 
