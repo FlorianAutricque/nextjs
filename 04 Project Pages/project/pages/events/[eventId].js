3474401f40521f1aef60acf9be43703b4c63b2cb
florian autricque <fautricque@gmail.com>
2023-09-26 16:46:06 -0400

    refactoring after data firebase

--- 04 Project Pages/project/pages/events/[eventId].js
+++ 04 Project Pages/project/pages/events/[eventId].js
@@ -2,15 +2,11 @@ import EventContent from "@/components/event-detail/event-content";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventSummary from "@/components/event-detail/event-summary";
 import ErrorAlert from "@/components/ui/error-alert";
-import { getEventById } from "@/dummy-data";
-import { useRouter } from "next/router";
+import { getEventById, getAllEvents } from "../../helpers/api-util";
 import { Fragment } from "react";
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage({ selectedEvent }) {
+  const event = selectedEvent;
 
   if (!event) {
     return (
@@ -34,6 +30,29 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+
+  const paths = events.map(event => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
 }
 
 export default EventDetailPage;
--- 04 Project Pages/project/pages/index.js
+++ 04 Project Pages/project/pages/index.js
@@ -1,14 +1,22 @@
 import EventList from "@/components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+function HomePage({ events }) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
 
 export default HomePage;
