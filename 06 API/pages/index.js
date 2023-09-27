9ff9b00bc60df8f845c758f7c322830cdad85327
florian autricque <fautricque@gmail.com>
2023-09-27 16:11:31 -0400

    form

--- 06 API/pages/index.js
+++ 06 API/pages/index.js
@@ -1,7 +1,47 @@
+import { useRef, useState } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  const [errorEmail, setErrorEmail] = useState(null);
+  const [errorFeedback, setErrorFeedback] = useState(null);
+
+  function submitFormHandler(e) {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    if (!enteredEmail) {
+      setErrorEmail("Please provide an email");
+    } else {
+      setErrorEmail(null);
+    }
+
+    if (!enteredFeedback) {
+      setErrorFeedback("Please provide a feedback");
+    } else {
+      setErrorFeedback(null);
+    }
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+          {errorEmail && <p>{errorEmail}</p>}
+        </div>
+
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+          {errorFeedback && <p>{errorFeedback}</p>}
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
