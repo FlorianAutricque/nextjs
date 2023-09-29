6ff0aa82552e8675873cc8dff726b2f223a8d54e
florian autricque <fautricque@gmail.com>
2023-09-29 15:11:48 -0400

    usecontext nextjs

--- 04 Project Pages/project/pages/_app.js
+++ 04 Project Pages/project/pages/_app.js
@@ -1,7 +1,6 @@
 import Layout from "@/components/layout/layout";
 import "../styles/globals.css";
 import Head from "next/head";
-import Notification from "@/components/ui/notification";
 
 import { NotificationContextProvider } from "../store/notification-context";
 
@@ -18,7 +17,6 @@ function MyApp({ Component, pageProps }) {
           />
         </Head>
         <Component {...pageProps} />;
-        <Notification title="Test" message="this is a test" status="pending" />
       </Layout>
     </NotificationContextProvider>
   );
--- 04 Project Pages/project/store/notification-context.js
+++ 04 Project Pages/project/store/notification-context.js
@@ -1,14 +1,48 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NotificationContext = createContext({
   notification: null,
-  showNotifaction: function () {},
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider({ children }) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>{children}</NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
+      {children}
+    </NotificationContext.Provider>
   );
 }
 
