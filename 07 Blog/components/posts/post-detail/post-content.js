1b4400fcbcb97c7269c94194f380c5110222206d
florian autricque <fautricque@gmail.com>
2023-10-03 16:44:22 -0400

    markdown

--- 07 Blog/components/posts/post-detail/post-content.js
+++ 07 Blog/components/posts/post-detail/post-content.js
@@ -3,21 +3,13 @@ import styles from "./post-content.module.css";
 
 import ReactMarkdown from "react-markdown";
 
-const DUMMY_POST = {
-  slug: "getting-ok2",
-  title: "getting ok with nextjs",
-  image: "next.png",
-  date: "2022-02-12",
-  content: "# This is a first post",
-};
-
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent({ post }) {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={styles.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
