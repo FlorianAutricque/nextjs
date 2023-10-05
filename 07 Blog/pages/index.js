a46157562b97963e5d042c5012d0c2110e25c253
florian autricque <fautricque@gmail.com>
2023-10-05 13:53:56 -0400

    document and end of project

--- 07 Blog/pages/index.js
+++ 07 Blog/pages/index.js
@@ -1,11 +1,16 @@
 import FeaturedPosts from "@/components/home-page/featured-posts";
 import Hero from "@/components/home-page/hero";
 import { getFeaturedPosts } from "@/lib/posts-utils";
+import Head from "next/head";
 import { Fragment } from "react";
 
 function HomePage({ posts }) {
   return (
     <Fragment>
+      <Head>
+        <title>Flo's Blog</title>
+        <meta name="description" content="I post about web development" />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts} />
     </Fragment>
--- 07 Blog/pages/posts/[slug].js
+++ 07 Blog/pages/posts/[slug].js
@@ -1,8 +1,18 @@
 import { getPostData, getPostsFiles } from "@/lib/posts-utils";
 import PostContent from "../../components/posts/post-detail/post-content";
+import { Fragment } from "react";
+import Head from "next/head";
 
 function PostDetailPage({ post }) {
-  return <PostContent post={post} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.excerpt} />
+      </Head>
+      <PostContent post={post} />;
+    </Fragment>
+  );
 }
 
 export function getStaticProps(context) {
