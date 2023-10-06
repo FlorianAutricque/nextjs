0f116696e7288886f1f16b1dcbcd3d290180aad5
florian autricque <fautricque@gmail.com>
2023-10-06 15:44:06 -0400

    creating user

--- 08 Authentification/pages/api/auth/signup.js
+++ 08 Authentification/pages/api/auth/signup.js
@@ -2,34 +2,36 @@ import { hashPassword } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
 
 async function handler(req, res) {
-  const data = req.body;
+  if (req.method === "POST") {
+    const data = req.body;
 
-  const { email, password } = data;
+    const { email, password } = data;
 
-  if (
-    !email ||
-    !email.imcules("@") ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res.status(422).json({
-      message:
-        "Ivalid input - password should also be at least 7 characters long",
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message:
+          "Ivalid input - password should also be at least 7 characters long",
+      });
+    }
+
+    const client = await connectToDatabase();
+
+    const db = client.db();
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection("users").insertOne({
+      email: email,
+      password: hashedPassword,
     });
+
+    res.status(201).json({ message: "Created user!" });
   }
-
-  const client = await connectToDatabase();
-
-  const db = client.db();
-
-  const hashedPassword = hashPassword(password);
-
-  db.collection("users").insertOne({
-    email: email,
-    password: hashedPassword,
-  });
-
-  res.status(201).json({ message: "Created user!" });
 }
 
 export default handler;
