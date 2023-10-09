d21b9e1885ab9b9520af1a5956554b05227e1f71
florian autricque <fautricque@gmail.com>
2023-10-09 19:13:13 -0400

    modification

--- 08 Authentification/lib/db.js
+++ 08 Authentification/lib/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-    "mongodb+srv://florian:U1a8wwJKBNInojiQ@cluster0.qf3qtyt.mongodb.net/auth?retryWrites=true&w=majority"
+    "mongodb+srv://florian:e4udYA6rA7IyUtwd@cluster0.qf3qtyt.mongodb.net/auth?retryWrites=true&w=majority"
   );
   return client;
 }
